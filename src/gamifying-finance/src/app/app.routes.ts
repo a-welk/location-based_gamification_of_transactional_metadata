51cdeebd1857df5c0170d911721e475bb61cffeb
Daniel <tdanielhuynh@gmail.com>
2023-11-13 13:04:10 -0500

    Generated components and linked routes

--- src/gamifying-finance/src/app/app.routes.ts
+++ src/gamifying-finance/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'leaderboard', component: LeaderboardComponent}
+];
--- src/gamifying-finance/src/app/navbar/navbar.component.ts
+++ src/gamifying-finance/src/app/navbar/navbar.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LeaderboardComponent } from '../leaderboard/leaderboard.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DashboardComponent, LeaderboardComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isCollapsed : boolean = true;
 
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  ngOnInit() {
+
+  }
 }

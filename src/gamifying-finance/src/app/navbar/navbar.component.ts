3639120b0432a3c951ad3cc50df37ac4513543ad
Daniel <tdanielhuynh@gmail.com>
2024-02-08 21:37:00 -0500

    Dialog devopment

--- src/gamifying-finance/src/app/navbar/navbar.component.ts
+++ src/gamifying-finance/src/app/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { LeaderboardComponent } from '../leaderboard/leaderboard.component';
 import { LoginComponent } from '../login/login.component';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
+import { SignupComponent } from '../signup/signup.component';
+import { DialogService } from '../services/dialog.service';
 
 @Component({
   selector: 'app-navbar',
@@ -22,11 +25,21 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn$!: Observable<boolean>;
+  loginDialogRef!: MatDialogRef<LoginComponent>;
+  signupDialogRef!: MatDialogRef<SignupComponent>;
 
-  constructor(public AuthService: AuthService) {
+  constructor(public AuthService: AuthService, private dialogService: DialogService) {
   }
 
   ngOnInit() {
     this.isLoggedIn$ = this.AuthService.isLoggedIn();
+  }
+
+  openLoginDialog(): void {
+    this.dialogService.openLoginDialog();
+  }
+
+  openSignupDialog(): void {
+    this.dialogService.openSignupDialog();
   }
 }

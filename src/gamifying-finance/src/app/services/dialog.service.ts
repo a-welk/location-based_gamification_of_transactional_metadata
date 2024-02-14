28d377fb1d508f2c0c457b5a3018e388d0e119fc
Daniel <tdanielhuynh@gmail.com>
2024-02-13 23:33:34 -0500

    code clean up & login

--- src/gamifying-finance/src/app/services/dialog.service.ts
+++ src/gamifying-finance/src/app/services/dialog.service.ts
@@ -14,17 +14,15 @@ export class DialogService {
   openLoginDialog(): void {
     if (!this.isDialogOpen) {
       const dialogConfig = new MatDialogConfig();
-      dialogConfig.width = '400px'; // Adjust the width as needed
+      dialogConfig.width = '400px';
       dialogConfig.height = '400px';
       dialogConfig.position = { top: '-33%', left: '35%' };
       dialogConfig.panelClass = 'custom-dialog-container';
       dialogConfig.autoFocus = false;
       const dialogRef = this.dialog.open(LoginComponent, dialogConfig);
 
-      // Set dialog state to open
       this.isDialogOpen = true;
 
-      // Subscribe to dialog close event to set dialog state to closed
       dialogRef.afterClosed().subscribe(() => {
         this.isDialogOpen = false;
       });
@@ -34,17 +32,14 @@ export class DialogService {
   openSignupDialog(): void {
     if (!this.isDialogOpen) {
       const dialogConfig = new MatDialogConfig();
-      dialogConfig.width = '400px'; // Adjust the width as needed
+      dialogConfig.width = '400px';
       dialogConfig.height = '400px';
       dialogConfig.position = { top: '-33%', left: '35%' };
       dialogConfig.panelClass = 'custom-dialog-container';
-      dialogConfig.autoFocus = false;
       const dialogRef = this.dialog.open(SignupComponent, dialogConfig);
 
-      // Set dialog state to open
       this.isDialogOpen = true;
 
-      // Subscribe to dialog close event to set dialog state to closed
       dialogRef.afterClosed().subscribe(() => {
         this.isDialogOpen = false;
       });
